9d108f61f2b2a3c94a9572cb08d6d7972eea25e5
Anna Oksanichenko <oksanichenkoann@gmail.com>
2020-12-21 12:26:09 +0200

    working on flips

--- TasteApp/ViewControllers/GameViewController.swift
+++ TasteApp/ViewControllers/GameViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class GameViewController: ViewController {
+class GameViewController: UIViewController {
 
-    
+    @IBOutlet weak var buttonCard: UIButton!
+    var isOpen = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +18,28 @@ class GameViewController: ViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func buttonFlip(_ sender: UIButton) {
+        
+        if isOpen {
+            isOpen = false
+         
+            let title = "Summer"
+            buttonCard.setTitle(title, for: .normal)
+            UIView.transition(with: buttonCard, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+            //   let image = UIImage(named: "summer")
+//            buttonCard.setImage(image, for: .normal)
+//            UIView.transition(with: buttonCard, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        } else {
+            isOpen = true
+//            let image = UIImage(named: "winter")
+//            buttonCard.setImage(image, for: .normal)
+//            UIView.transition(with: buttonCard, duration: 0.3, options: .transitionFlipFromRight, animations: nil, completion: nil)
+            let title = "Winter"
+            buttonCard.setTitle(title, for: .normal)
+            UIView.transition(with: buttonCard, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        }
+    }
+    
     
 
 }
