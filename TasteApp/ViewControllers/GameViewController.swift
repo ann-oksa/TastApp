eb19a195e7b4ddfa5489315168f2a66b7576f3bf
Anna Oksanichenko <oksanichenkoann@gmail.com>
2020-12-21 18:07:17 +0200

    started working on another cards in game

--- TasteApp/ViewControllers/GameViewController.swift
+++ TasteApp/ViewControllers/GameViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 class GameViewController: UIViewController {
 
     @IBOutlet weak var buttonCard: UIButton!
-    var isOpen = false
     
+    var isOpen = false
+    let shared = AppState.shared
+    var index = 0
+    var cardTitle: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBAction func buttonFlip(_ sender: UIButton) {
@@ -23,8 +26,11 @@ class GameViewController: UIViewController {
         if isOpen {
             isOpen = false
          
-            let title = "Summer"
-            buttonCard.setTitle(title, for: .normal)
+//            let title = "Summer"
+             cardTitle = shared.history.journal[index].word1
+            buttonCard.setTitle(cardTitle, for: .normal)
+            buttonCard.backgroundColor  = .lightGray
+            
             UIView.transition(with: buttonCard, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
             //   let image = UIImage(named: "summer")
 //            buttonCard.setImage(image, for: .normal)
@@ -34,12 +40,21 @@ class GameViewController: UIViewController {
 //            let image = UIImage(named: "winter")
 //            buttonCard.setImage(image, for: .normal)
 //            UIView.transition(with: buttonCard, duration: 0.3, options: .transitionFlipFromRight, animations: nil, completion: nil)
-            let title = "Winter"
-            buttonCard.setTitle(title, for: .normal)
+//            let title = "Winter"
+             cardTitle = shared.history.journal[index].word2
+
+            buttonCard.setTitle(cardTitle, for: .normal)
+            buttonCard.backgroundColor = .white
             UIView.transition(with: buttonCard, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
         }
     }
     
+    @IBAction func nextClicked(_ sender: UIButton) {
+        index += 1
+        
+    }
     
-
+    @IBAction func previousClicked(_ sender: UIButton) {
+    }
+    
 }
